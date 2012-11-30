54b0a437185e27d54d1863cfb2cd85898947aacc
Ian Gilman <ian@iangilman.com>
2012-11-30 15:56:07 -0800

    Special "all" tag

--- tag-view/js/collection-view.js
+++ tag-view/js/collection-view.js
@@ -9,6 +9,7 @@
       this.$albums = this.$el.find('.albums .inner');
       this.$tags = this.$el.find('.tags .inner');
       this.albumViews = [];
+      this.selectedTag = null;
       
       this.$tags.on("click", ".tag", function(event) {
         var $target = $(event.target);
@@ -37,21 +38,32 @@
     
     renderTags: function() {
       var self = this;
-      self.$tags.empty();
+      
+      this.$tags.empty();
+      
+      this.renderTag('all', Main.collection.length, null);
+      
       Main.tags.sort();
       Main.tags.each(function(v, i) {
         var count = v.get('count');
         if (count > 1) {
-          var $tag = $('<p class="tag">' + v.get('name') + ' (' + count + ')</p>')
-            .data('tag', v)
-            .appendTo(self.$tags);
+          self.renderTag(v.get('name'), count, v);
         }
       });
     },
     
+    renderTag: function(name, count, tag) {
+      var $tag = $('<p class="tag">' + name + ' (' + count + ')</p>')
+        .data('tag', tag)
+        .toggleClass('selected', this.selectedTag == tag)
+        .appendTo(this.$tags);
+    },
+    
     selectTag: function(tag) {
+      this.selectedTag = tag;
       _.each(this.albumViews, function(v, i) {
-        v.toggle(tag.get('albums').indexOf(v.model) != -1);
+        var show = (!tag || tag.get('albums').indexOf(v.model) != -1);
+        v.toggle(show);
       });
     }
   };
