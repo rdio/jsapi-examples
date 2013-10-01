525bcc65e51cde66e4aacf48e9536cff0668ead4
Ian Gilman <ian@iangilman.com>
2013-09-30 17:04:08 -0700

    Album widget now uses R.router.navigate()

--- utils/development/src/album-widget.js
+++ utils/development/src/album-widget.js
@@ -6,6 +6,17 @@
 (function(R, rdioUtils) {
 
   // ----------
+  var linkClickHandler = function(event) {
+    var isMac = /mac/i.test(navigator.userAgent);
+    if ((!isMac && event.ctrlKey) || (isMac && event.metaKey)) {
+      return;
+    }
+
+    rdioUtils._stopEvent(event);
+    R.router.navigate(event.currentTarget.href);
+  };
+
+  // ----------
   rdioUtils.AlbumWidget = function(album) {
     this._element = document.createElement('div');
     this._element.className = 'rdio-utils-album';
@@ -28,7 +39,7 @@
 
     this._element.innerHTML = ''
         + '<div class="rdio-utils-album-cover">'
-          + '<a href="http://www.rdio.com' + rdioUtils._escape(album.url) + '" target="_blank">'
+          + '<a href="http://www.rdio.com' + rdioUtils._escape(album.url) + '">'
             + '<div class="rdio-utils-album-icon" style="background-image: url(' + rdioUtils._escape(album.icon) + ')"></div>'
             + '<div class="rdio-utils-album-hover-overlay">'
               + '<div class="rdio-utils-album-play-btn"></div>'
@@ -36,9 +47,14 @@
             + '</div>'
           + '</a>'
         + '</div>'
-        + '<div class="rdio-utils-album-title rdio-utils-truncated"><a href="http://www.rdio.com' + rdioUtils._escape(album.url) + '" target="_blank">' + rdioUtils._escape(album.name) + '</a></div>'
-        + '<div class="rdio-utils-album-author rdio-utils-truncated"><a href="http://www.rdio.com' + rdioUtils._escape(album.artistUrl) + '" target="_blank">' + rdioUtils._escape(album.artist) + '</a></div>'
+        + '<div class="rdio-utils-album-title rdio-utils-truncated"><a href="http://www.rdio.com' + rdioUtils._escape(album.url) + '">' + rdioUtils._escape(album.name) + '</a></div>'
+        + '<div class="rdio-utils-album-author rdio-utils-truncated"><a href="http://www.rdio.com' + rdioUtils._escape(album.artistUrl) + '">' + rdioUtils._escape(album.artist) + '</a></div>'
         + '<div class="rdio-utils-album-size rdio-utils-truncated">' + rdioUtils._escape(album.length) + ' songs</div>';
+
+    var links = this._element.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+      rdioUtils._bind(links[i], 'click', linkClickHandler);
+    }
 
     var button = this._element.getElementsByClassName('rdio-utils-album-play-btn')[0];
     rdioUtils._bind(button, 'click', function(event) {
