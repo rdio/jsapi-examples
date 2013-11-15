2b46f980837f94a78179ace85b6a7117ed2db325
Ian Gilman <ian@iangilman.com>
2013-11-15 09:43:31 -0800

    Touch events now work

--- mobile-queue/js/queue-item.js
+++ mobile-queue/js/queue-item.js
@@ -35,8 +35,26 @@
       var self = this;
       var drag = null;
 
+      var downEventName = (Modernizr.touch) ? 'touchstart' : 'mousedown';
+      var upEventName = (Modernizr.touch) ? 'touchend' : 'mouseup';
+      var moveEventName = (Modernizr.touch) ? 'touchmove' : 'mousemove';
+      
       var moveHandler = function(event) {
-        var offset = event.clientY - drag.startY;
+        var y;
+
+        if (Modernizr.touch) {
+          var touches = event.originalEvent.changedTouches;
+          var touch = _.findWhere(touches, { identifier: drag.id });
+          if (!touch) {
+            return;
+          }
+
+          y = touch.clientY;
+        } else {
+          y = event.clientY;
+        }
+
+        var offset = y - drag.startY;
         self.$el.css({
           top: offset
         });
@@ -47,32 +65,58 @@
           // console.log(count, offset);
           self.queue.shiftDown(self.queue.index(self) - 1, count);
         }
+
+        event.preventDefault();
+        event.stopPropagation();
       };
 
       var upHandler = function(event) {
+        if (Modernizr.touch) {
+          var touches = event.originalEvent.changedTouches;
+          var touch = _.findWhere(touches, { identifier: drag.id });
+          if (!touch) {
+            return;
+          }
+        }
+
         self.queue.move(self, drag.shift);
 
         self.$el.css({
           top: 0
         });
 
-        self.$el.unbind('mousemove', moveHandler);
-        self.$el.unbind('mouseup', upHandler);
+        self.$el.unbind(moveEventName, moveHandler);
+        self.$el.unbind(upEventName, upHandler);
         self.$el.removeClass('dragging');
         drag = null;
+
+        event.preventDefault();
+        event.stopPropagation();
       };
 
       this.$el.find('.grip')
-        .mousedown(function(event) {
+        .bind(downEventName, function(event) {
           drag = {
-            startX: event.clientX,
-            startY: event.clientY,
             shift: 0
           };
 
-          self.$el.mousemove(moveHandler);
-          self.$el.mouseup(upHandler);
+          if (Modernizr.touch) {
+            var touches = event.originalEvent.changedTouches;
+            if (!touches.length) {
+              return;
+            }
+
+            drag.startY = touches[0].clientY;
+            drag.id = touches[0].identifier;
+          } else {
+            drag.startY = event.clientY;            
+          }
+
+          self.$el.bind(moveEventName, moveHandler);
+          self.$el.bind(upEventName, upHandler);
           self.$el.addClass('dragging');
+          event.preventDefault();
+          event.stopPropagation();
         });
     }
   };
