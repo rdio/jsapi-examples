70c4d9c9b5c56ba285a2f8ddc0955124d103c962
Ian Gilman <ian@iangilman.com>
2013-11-27 14:33:32 -0800

    Added Jasmine

--- utils/development/Gruntfile.js
+++ utils/development/Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
     // ----------
     var packageJson = grunt.file.readJSON('package.json'),
